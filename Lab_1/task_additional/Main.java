846ac0580e9b978799194acd15dfcbca8c6c2164
savukhin <savukhin@gmail.com>
2023-03-06 19:40:26 +0300

    Updated additional task

--- Lab_1/task_additional/Main.java
+++ Lab_1/task_additional/Main.java
@@ -6,13 +6,21 @@ import java.util.List;
 public class Main {
     public static void main(String[] args) {
         List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        String str = method(list);
-        System.out.println(str);
+        try {
+            String str = method(list, 10);
+            System.out.println(str);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());   
+        }
     }
 
-    public static String method(List<Integer> list) {
+    public static String method(List<Integer> list, int size) throws IllegalArgumentException {
+        if (size > list.size()) {
+            throw new IllegalArgumentException("list size smaller than desired size");
+        }
+
         StringBuilder builder = new StringBuilder();
-        for (int i=0; i < list.size(); i++) {
+        for (int i=0; i < size; i++) {
             builder.append(" ").append(list.get(i));
         }
         
