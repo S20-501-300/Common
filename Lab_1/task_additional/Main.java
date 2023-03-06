21281fa5320180d31c696140b289c7175668b1ae
savukhin <savukhin@gmail.com>
2023-03-06 19:54:18 +0300

    Edited additional task

--- Lab_1/task_additional/Main.java
+++ Lab_1/task_additional/Main.java
@@ -1,29 +1,29 @@
 package task_additional;
 
 import java.util.Arrays;
-import java.util.List;
+
+// import java.util.Arrays;
+// import java.util.List;
 
 public class Main {
-    public static void main(String[] args) {
-        List<Integer> list = Arrays.asList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
-        try {
-            String str = method(list, 15);
-            System.out.println(str);
-        } catch (IndexOutOfBoundsException e) {
-            System.out.println("Error: " + e.getMessage());   
-        }
-    }
-
-    public static String method(List<Integer> list, int size) throws IndexOutOfBoundsException {
-        if (size > list.size()) {
-            throw new IndexOutOfBoundsException("list size smaller than desired size");
-        }
-
+    public static String method(int[] arr, int size){
         StringBuilder builder = new StringBuilder();
+        builder.append("[");
         for (int i=0; i < size; i++) {
-            builder.append(" ").append(list.get(i));
+            if (i > 0)
+                builder.append(", ");
+                
+            builder.append(arr[i]);
         }
+        builder.append("]");
         
         return builder.toString();
+    }
+
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+        String str= method(arr, 4);
+        System.out.println(str);
+        System.out.println(Arrays.toString(arr));
     }
 }
