ef20da695c3cb9eb58b0efe520154bf09730dcef
savukhin <savukhin@gmail.com>
2023-03-20 17:51:29 +0300

    Added task 2

--- Lab_2/Task2.java
+++ Lab_2/Task2.java
@@ -1,2 +1,25 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Task2 {
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        /*  Метод nextInt модифицирующий, так как он меняет значение полей объекта
+        Внутри есть команда setRadix(radix); которая изменяет внутреннее значение сканнера
+        */
+
+
+        int n = in.nextInt();
+        in.close();
+
+        // ============================================================ //
+        // ############################################################ //
+        // ============================================================ //
+        
+        Random random = new Random();
+
+        // Метод nextInt - метод доступа, так как он не меняет объекта
+        int s = random.nextInt();
+    }
+
 }
