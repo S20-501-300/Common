a558eb6cf070755fe28ab66a8ef0c806ddfbcb0f
makardom <70172017+makardom@users.noreply.github.com>
2023-05-07 14:40:58 +0300

    Create Unit.java

---
+++ Lab_3/Comparator/Unit.java
@@ -0,0 +1,14 @@
+public class Unit {
+    final private int measure;
+    final private int second;
+    Unit(int a, int b){
+        this.measure = a;
+        this.second = b;
+    }
+    public int getMeasure(){
+        return measure;
+    }
+    public int getSecond(){
+        return second;
+    }
+}
